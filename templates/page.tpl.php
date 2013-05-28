6edb8ae6d190308baf4cbf56c07d50e99eaf0824
refaktorco <refaktor.co@gmail.com>
2013-05-28 14:58:41 -0700

    -Very stripped down page template.

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -48,142 +48,92 @@
 	     <!-- END BRANDING RIGHT BLOCK REGION -->   
 	  </section>
 	  
-	  <div class="row">
-	    <div class="span12">
-	      <div class="menu-wrap">
-	        <div class="row">
-				    <div class="span9">    
-						  <nav class="main-menu-nav">
-							  <?php if (isset($page['menu'])) { print render($page['menu']); } ?>
-						  </nav>
-				    </div>
+	  <div class="container" id="navbar">
+	    <section class="row">
+	      <!-- NAVIGATION -->
+	      <div class="eight columns">
+		      <nav id="navigationmain">
+            <?php if (isset($page['header_memu_left'])) { print render($page['header_menu_left']); } ?>
+					</nav>
+				</div>
 				    
-				    <div class="span3">
-				      <?php if (isset($page['menu_social'])) { print render($page['menu_social']); } ?>
-				    </div>
-	        </div>
-	      </div>    
-	    </div>  
-	  </div>  
-	  
-	<?php //expressa_style_switch(); ?>  
-	</header>
-	
-	<?php if ($breadcrumb): ?>
-	<div id="page-heading" class="row">
-	  <div class="span12">
-	    <h2><?php  print drupal_get_title(); ?></h2>
-	    <?php if (theme_get_setting('breadcrumbs') == '1'): ?>
-	      <div id="breadcrumbs"><?php print $breadcrumb; ?> </div>	
-	    <?php endif; ?>  	   
-	    <hr>
+		    <div class="four columns">
+		      <?php if (isset($page['header_menu_right'])) { print render($page['header_menu_right']); } ?>
+		    </div>
+	    </section>  
 	  </div>
-	</div>       
-	<?php endif; ?>
-	
-	<div class="before-content"></div>
-	
-	
-  <div class="row">
-    <div class="<?php if ($page['sidebar_first']) { echo "span9";} else { echo "span12"; } ?>">
-    
-      <?php print render($title_prefix); ?>
-      <?php print render($title_suffix); ?>
-     	 
-     	<?php if ($tabs = render($tabs)): ?>
-		    <div id="drupal_tabs" class="tabs bigpadding">
-		      <?php print render($tabs); ?>
-		    </div>
-		  <?php endif; ?>
-      <?php print render($page['help']); ?>
-      <?php if ($action_links): ?>
-        <ul class="action-links">
-          <?php print render($action_links); ?>
-        </ul>
-      <?php endif; ?>
-
-		  <?php if (isset($page['content'])) : ?>
-		    <?php print render($page['content']); ?>
-		  <?php endif; ?>  
-  
-    </div>
-    <?php if (isset($page['sidebar_first'])) : ?>
-    <div class="span3">
-      <?php print render($page['sidebar_first']); ?>
-    </div>
-    <?php endif; ?>
-    
-  </div><!-- end page content row -->  
-	  
-  <div class="row">
-    <div class="span12">
-      <?php print render($page['after_content']); ?>
-    </div>
   </div>  
-	 
-  <?php print $messages; ?>
-  </div>
-</div> <!-- End Content Wrap -->
-
-<div id="footer-wrap">
-  <?php if (isset($page['highlight'])) : ?>
-    
-    <div class="arrow-down"></div>
-    <div id="highlight">
-      <?php print render($page['highlight']); ?>
-    </div>
-
-  <?php endif; ?>
-  <footer>  
-    <div id="primary-footer" class="container">
-    
-		  <div class="row">
-		  
-		    <div class="span3">
-		      <?php if (isset($page['footer_col_1'])) : ?>
-				    <?php print render($page['footer_col_1']); ?>
-				  <?php endif; ?>
-		    </div>
-		    
-		    <div class="span3">
-		      <?php if (isset($page['footer_col_2'])) : ?>
-				    <?php print render($page['footer_col_2']); ?>
-				  <?php endif; ?>
-		    </div>
-		    
-		    <div class="span3">
-		      <?php if (isset($page['footer_col_3'])) : ?>
-				    <?php print render($page['footer_col_3']); ?>
-				  <?php endif; ?>
-		    </div>
-		    
-		    <div class="span3">
-		      <?php if (isset($page['footer_col_4'])) : ?>
-				    <?php print render($page['footer_col_4']); ?>
-				  <?php endif; ?>
-		    </div>
-		    
-		  </div>  
-    </div>
-	  <div id="secondary-footer">  
-	    <div class="container">
-	      <div class="row">
-			    <div class="span6">
-			    
-					  <?php if (isset($page['footer_bottom_left'])) : ?>
-					    <?php print render($page['footer_bottom_left']); ?>
-					  <?php endif; ?>
-			  
-			    </div>
-			    <div class="span6">
-			    
-					  <?php if (isset($page['footer_bottom_right'])) : ?>
-					    <?php print render($page['footer_bottom_right']); ?>
-					  <?php endif; ?>
-			  
-			    </div>
-	      </div>  
+</header>
+<div class="row">
+  <div class="<?php if ($page['sidebar_first']) { echo "eight columns";} else { echo "twelve columns"; } ?>">
+  
+    <?php print render($title_prefix); ?>
+    <?php print $title; ?>
+    <?php print render($title_suffix); ?>
+   	 
+   	<?php if ($tabs = render($tabs)): ?>
+	    <div id="drupal_tabs" class="tabs bigpadding">
+	      <?php print render($tabs); ?>
 	    </div>
-	  </div>  
-	</footer>
+	  <?php endif; ?>
+	  
+    <?php print render($page['help']); ?>
+    <?php if ($action_links): ?>
+      <ul class="action-links">
+        <?php print render($action_links); ?>
+      </ul>
+    <?php endif; ?>
+
+	  <?php if (isset($page['content'])) : ?>
+	    <?php print render($page['content']); ?>
+	  <?php endif; ?>  
+
+  </div>
+  
+  <?php if (isset($page['sidebar_first'])) : ?>
+  <div class="four columns">
+    <?php print render($page['sidebar_first']); ?>
+  </div>
+  <?php endif; ?>
+<?php print $messages; ?>
 </div>
+<footer>
+  <div class="black container top largetoppadding midbottompadding">
+		<div class="row bigbottompadding">
+	    <div class="four columns">
+	      <?php if (isset($page['footer_col_1'])) : ?>
+			    <?php print render($page['footer_col_1']); ?>
+			  <?php endif; ?>
+	    </div>
+	    <div class="three columns">
+	      <?php if (isset($page['footer_col_2'])) : ?>
+			    <?php print render($page['footer_col_2']); ?>
+			  <?php endif; ?>
+	    </div>
+	    <div class="three columns">
+	      <?php if (isset($page['footer_col_3'])) : ?>
+			    <?php print render($page['footer_col_3']); ?>
+			  <?php endif; ?>
+	    </div>
+	    <div class="two columns">
+	      <?php if (isset($page['footer_col_4'])) : ?>
+			    <?php print render($page['footer_col_4']); ?>
+			  <?php endif; ?>
+	    </div>
+		</div>  
+  </div>
+	<div class="black container midpadding">
+	  <section class="row smalltoppadding">			    
+	    <div class="six columns">
+			  <?php if (isset($page['footer_bottom_left'])) : ?>
+			    <?php print render($page['footer_bottom_left']); ?>
+			  <?php endif; ?>
+      </div>
+			<div class="six columns">
+			  <?php if (isset($page['footer_bottom_right'])) : ?>
+			    <?php print render($page['footer_bottom_right']); ?>
+			  <?php endif; ?>			    
+			</div>
+	  </section>  
+	</div>  
+</footer>
