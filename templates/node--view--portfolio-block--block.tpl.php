86d9830b0d99fbe3fc81b7fea0096db7c4c62b72
refaktorco <refaktor.co@gmail.com>
2013-06-05 14:39:24 -0700

    -Working on Portfolio Block

---
+++ templates/node--view--portfolio-block--block.tpl.php
@@ -0,0 +1,18 @@
+<!-- PORTFOLIO ITEM -->
+<div class="three columns item">
+	<img src="<?php echo file_create_url($node->field_image['und'][0]['uri']); ?>" alt="item">
+	
+	<!-- prettyphoto link -->																																																														
+	<a href="<?php echo file_create_url($node->field_image['und'][0]['uri']); ?>" data-rel="prettyPhoto" title="<?php print $title; ?>">
+	<div class="item-icon">
+		<i class="icon-zoom-in"></i>
+	</div>
+	</a>
+	<!-- view project link with project title and project categories -->
+	<a href="itembasic.html" title="click to view project">
+	<div class="item-info">
+		<h6 class="blacktext"><?php print $title; ?></h6>
+		<h6 class="greytext smallfont"><?php print render($content['field_portfolio_category']); ?></h6>
+	</div>
+	</a>
+</div>
