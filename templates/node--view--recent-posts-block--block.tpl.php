f6aeb48b6473ed516cb0be37f6adfc2e7931ab28
refaktorco <refaktor.co@gmail.com>
2013-06-05 16:02:15 -0700

    -Just tweaking the recent posts block and the front page is finished.

---
+++ templates/node--view--recent-posts-block--block.tpl.php
@@ -0,0 +1,27 @@
+<!-- post one -->
+<article class="home-blog-post">
+<div class="content-title">
+	<!-- post date -->
+	<div class="home-post-meta">
+		<span class="day"><?php print format_date($node->created, 'custom', 'd'); ?></span><span class="month"><?php print format_date($node->created, 'custom', 'M'); ?><</span>
+	</div>
+	<!-- post title and blog category -->
+	<div class="post-title">
+		<h6 class="blacktext bold"><?php print $title; ?></h6>
+		<h6 class="blacktext meta">POSTED UNDER: <?php print render($content['field_tags']); ?></h6>
+	</div>
+</div>
+<!-- post excerpt -->
+<p class="content-post">
+	  <?php
+      hide($content['field_tags']);
+      hide($content['field_image']);
+      print render($content);
+    ?>
+    
+    <?php if($teaser): ?>
+  	<a class="small button" href="<?php print $node_url;?>">read more</a>
+<?php endif;?>
+</p>
+
+</article>
