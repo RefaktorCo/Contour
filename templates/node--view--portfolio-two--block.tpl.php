2a6a8f2e4dcf3c2ddcfa2ba0a69e3241f3b94c54
refaktorco <refaktor.co@gmail.com>
2013-06-07 12:38:27 -0700

    -nth:child magic for folio 2

---
+++ templates/node--view--portfolio-two--block.tpl.php
@@ -0,0 +1,17 @@
+<!-- PORTFOLIO ITEM for portfolio two we use class basic-->
+<div class="four columns basic">
+	<!-- image -->
+	<img src="<?php echo file_create_url($node->field_image['und'][0]['uri']); ?>" alt="item">
+	<!-- project info -->
+	<div class="basictitle">
+		<a href="<?php print $node_url;?>" title="Click to view project">
+		<h6 class="blacktext dosis bold caps"><?php print $title; ?></h6>
+		<h6 class="greytext smallfont caps"><?php print render($content['field_portfolio_category']); ?></h6>
+		</a>
+	</div>
+	
+	<!-- prettyphoto link -->
+	<div class="basicicon">
+		<a href="<?php echo file_create_url($node->field_image['und'][0]['uri']); ?>" data-rel="prettyPhoto" title="<?php print $title; ?>"><i class="icon-zoom-in whitetext"></i></a>
+	</div>
+</div>
