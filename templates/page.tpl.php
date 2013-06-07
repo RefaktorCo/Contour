440455348e1082f167d4c23e19dbb57bb6a14e2f
refaktorco <refaktor.co@gmail.com>
2013-06-07 08:44:00 -0700

    -Working on pages, page headings will use the before content block region, different block for every page.

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -98,6 +98,22 @@
 	  <?php endif; ?>
 
 		<div class="<?php if ( ($page['sidebar_right']) AND ($page['sidebar_left']) ) { echo "six columns";} elseif ( ($page['sidebar_right']) OR ($page['sidebar_left']) ) {  echo "nine columns"; }  else { echo "twelve columns"; } ?>">
+		    <?php print $messages; ?>
+		  <?php print render($title_prefix); ?>
+      <?php print render($title_suffix); ?>
+     	 
+     	<?php if ($tabs = render($tabs)): ?>
+		    <div id="drupal_tabs" class="tabs ">
+		      <?php print render($tabs); ?>
+		    </div>
+		  <?php endif; ?>
+      <?php print render($page['help']); ?>
+      <?php if ($action_links): ?>
+        <ul class="action-links">
+          <?php print render($action_links); ?>
+        </ul>
+      <?php endif; ?>
+
 		  <?php if (isset($page['content'])) { print render($page['content']); } ?>
 		</div>
   
@@ -107,9 +123,10 @@
 	  </div>
 	  <?php endif; ?>
   
-    <?php print $messages; ?>
+    
   </div>
 </div>
+
 <footer>
 <script>
 //<![CDATA[
