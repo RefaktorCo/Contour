c3487d1933a0c0d71708451328fa2128c24179d6
refaktorco <refaktor.co@gmail.com>
2013-06-07 14:56:05 -0700

    -Portfolio 3, just need to add like 5 more regular portfolio items to fill up the first template completely.

---
+++ templates/node--view--portfolio-three--block.tpl.php
@@ -0,0 +1,18 @@
+
+<!-- PORTFOLIO ITEM for portfolio 3 uses class item -->
+	<div class="three columns item">
+		<!-- image -->
+		<img src="<?php echo file_create_url($node->field_image['und'][0]['uri']); ?>" alt="item">
+		<!-- prettyphoto link -->
+		<div class="item-icon">
+			<i class="icon-zoom-in"></i>
+		</div>
+		</a>
+		<!-- item info -->
+		<a href="itembasic.html" title="click to view project">
+		<div class="item-info">
+			<h6 class="blacktext bold caps"><?php print $title; ?></h6>
+			<h6 class="greytext smallfont caps"><?php print render($content['field_portfolio_category']); ?></h6>
+		</div>
+		</a>
+	</div>
