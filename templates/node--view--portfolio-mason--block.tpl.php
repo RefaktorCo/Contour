e7d0cd9d5945e0e8e9b1b3b8a8f23b564f52e579
refaktorco <refaktor.co@gmail.com>
2013-06-10 13:59:45 -0700

    -Need to do other two mason templates and figure out how to manage another portfolio single template (may just go with the basic).

--- templates/node--view--portfolio-mason--block.tpl.php
+++ templates/node--view--portfolio-mason--block.tpl.php
@@ -1,6 +1,10 @@
-<?php $cat = render($content['field_portfolio_category']); ?>
+<?php
+$cat = render($content['field_portfolio_category']);
+$cat = strip_tags($cat);
+$cat = str_replace(' ', ' - ', $cat);
+?>
 
-<li class="box <?php print strip_tags($cat);  ?>">
+<li class="box <?php print strip_tags(render($content['field_portfolio_category'])); ?>">
 <!-- image -->
 <img src="<?php echo file_create_url($node->field_image['und'][0]['uri']); ?>" alt="box item">
 <!-- project info -->
@@ -8,7 +12,7 @@
 	<div class="text">
 		<a href="<?php print $node_url;?>" title="Click to view project">
 		<h5 class="bold whitetext big-shadow"><?php print $title; ?></h5>
-		<h6 class="light whitetext smallfont small-shadow"><?php print render($content['field_portfolio_category']); ?></h6>
+		<h6 class="light whitetext smallfont small-shadow caps"><?php print $cat; ?></h6>
 		</a>
 	</div>
 </div>
