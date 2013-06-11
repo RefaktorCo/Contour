d7d86261c41a13320d79bc9a2128cff711188b63
refaktorco <refaktor.co@gmail.com>
2013-06-11 14:47:26 -0700

    -Basic system in place to handle flag and unfledged color.

--- templates/flag--shop.tpl.php
+++ templates/flag--shop.tpl.php
@@ -37,7 +37,12 @@
 ?>
 <span class="<?php print $flag_wrapper_classes; ?>">
   <?php if ($link_href): ?>
-    <a href="<?php print $link_href; ?>" title="<?php print $link_title; ?>" class="<?php print $flag_classes ?>" rel="nofollow">TEST</a><span class="flag-throbber">&nbsp;</span>
+    <a href="<?php print $link_href; ?>" title="<?php print $link_title; ?>" class="<?php print $flag_classes ?>" rel="nofollow">
+    
+    <div class="wishlist-wrapper <?php print $flag_classes ?>">
+    <i class="icon-heart"></i>
+    </div>
+    </a><span class="flag-throbber">&nbsp;</span>
   <?php else: ?>
     <span class="<?php print $flag_classes ?>"><?php print $link_text; ?></span>
   <?php endif; ?>
