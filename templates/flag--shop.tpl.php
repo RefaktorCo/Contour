1f280bb100a2d326d855fa7cafca0b46919beb31
refaktorco <refaktor.co@gmail.com>
2013-06-17 09:58:15 -0700

    -Works, needed the span class around the link. Now just need to get it working with the style and do the custom color indicator or tooltip notification or something.

--- templates/flag--shop.tpl.php
+++ templates/flag--shop.tpl.php
@@ -36,14 +36,10 @@
  */
 ?>
 <span class="<?php print $flag_wrapper_classes; ?>">
-  <?php if ($link_href): ?>
-    <a href="<?php print $link_href; ?>" title="<?php print $link_title; ?>" class="<?php print $flag_classes ?>" rel="nofollow"><?php print $link_text; ?></a><span class="flag-throbber">&nbsp;</span>
-  <?php else: ?>
-    <span class="<?php print $flag_classes ?>"><?php print $link_text; ?></span>
-  <?php endif; ?>
-  <?php if ($after_flagging): ?>
-    <span class="flag-message flag-<?php print $status; ?>-message">
-      <?php print $message_text; ?>
-    </span>
-  <?php endif; ?>
+<a href="<?php print $link_href; ?>" class="<?php print $flag_classes ?>" title="Save product for later">
+  <div class="product-icon <?php print $flag_classes ?>">
+	  <i class="icon-heart"></i>
+  </div>
+</a>
+
 </span>
