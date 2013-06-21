6fb6512aa9bcea3011266df3b5c8c24e3dd9ec5d
refaktorco <refaktor.co@gmail.com>
2013-06-21 13:58:11 -0700

    -Working on ironing out these last color differences between white/black and black. I think a new class for the main heading item and then a new class for the buttons should do the trick.

--- templates/node/node--view--portfolio-block-page--block.tpl.php
+++ templates/node/node--view--portfolio-block-page--block.tpl.php
@@ -11,9 +11,9 @@
 	<img src="<?php echo file_create_url($node->field_image['und'][0]['uri']); ?>" alt="griditem">
 	<div class="info">
 		<h6 class="text-color caps largefont bold"><?php print $title; ?></h6>
-		<p class="greytext smallfont caps">
+		<h5 class="greytext smallfont caps">
 			<?php print render($content['field_portfolio_category']); ?>
-		</p>
+		</h5>
 	</div>
 	</a>
 </li>
